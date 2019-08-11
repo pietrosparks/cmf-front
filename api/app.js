9bf1c028abd078cf2099657e67f3345af5229a76
Theo <theo@Theos-MacBook-Air.local>
2019-08-11 11:15:14 +0100

    remove db

--- api/app.js
+++ api/app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
-const { connect } = require('./dbconfig/database')
 
 require('dotenv').load()
 require('./config')(app, express)
-// connect()
 
 // view engine setup
 
